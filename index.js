244a8e85b0390c782f152e4da4cec1e3e6aa69e0
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-01-27 22:31:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or2vp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Travel24");
+    const BlogCollection = database.collection("Blog");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to Travel24");
+});
+
+app.listen(port, () => {
+  //
+});
